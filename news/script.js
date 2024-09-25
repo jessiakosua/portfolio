bfa63f36f4b5a758f8525016e13daea0b3e9d291
jessiakosua <jessica.a.adika@gmail.com>
2024-09-25 11:56:17 +0000

    added new set

--- news/script.js
+++ news/script.js
@@ -10,12 +10,20 @@ fetch(
     for (const article of data.articles) {
       //console.log(article);
       const articleDiv = document.createElement("div");
+
       const articleH1 = document.createElement("h1");
       articleH1.textContent = article.title;
       articleDiv.appendChild(articleH1);
-      const articleP =document.createElement('p');
-      articleP . textContent =article.content;
-      articleDiv .appendChild(articleP);
+
+      const articleP = document.createElement("p");
+      articleP.textContent = article.description;
+      articleDiv.appendChild(articleP);
+
+      const articleImg = document.createElement("img");
+      articleImg.setAttribute("src", article.urlToImage);
+      articleImg.setAttribute("alt", article.title);
+      articleImg.classList.add("article-img");
+      articleDiv.appendChild(articleImg);
 
       document.querySelector("#articles").appendChild(articleDiv);
     }
